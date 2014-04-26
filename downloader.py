4bf205dc16676cd25395875994b529c2eaf38828
Fabio Teixeira <fabio1079@gmail.com>
2014-04-26 14:41:09 -0300

    Script to download all worksheets of 2007 and 2010

--- downloader.py
+++ downloader.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
-import urllib2
-
-#urllib2.urlopen("https://www.google.com")
+import os
 
 list_2007 = open("link_list/link_list_2007.txt")
+list_2010 = open("link_list/link_list_2010.txt")
 
-print list_2007
+for line in list_2007.readlines():
+	file_name, file_href = line.split(";")
+	os.system("wget -O ./2007/%s %s" % (file_name, file_href))
+
+for line in list_2010.readlines():
+	file_name, file_href = line.split(";")
+	os.system("wget -O ./2010/%s %s" % (file_name, file_href))
